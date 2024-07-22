eae482673ff6575690a6ba9eb3c872b689293ead
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-07-22 13:34:11 -0400

    spring config

---
+++ src/main/java/org/perscholas/BankOfAntMerica/BankOfAntMericaApplication.java
@@ -0,0 +1,13 @@
+package org.perscholas.BankOfAntMerica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfAntMericaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankOfAntMericaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/perscholas/BankOfAntMerica/BankOfAntMericaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.perscholas.BankOfAntMerica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankOfAntMericaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
