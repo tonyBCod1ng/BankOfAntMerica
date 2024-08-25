b3ec2f5e301ac907b17d6a5ff543f44a8a4a6f0d
Anthony Brown <75460744+tonyBCod1ng@users.noreply.github.com>
2024-08-25 19:50:04 -0400

    added preHandle to capture info about user for a feature.

--- src/main/java/org/perscholas/BankOfAntMerica/Config/YourInteceptor.java
+++ src/main/java/org/perscholas/BankOfAntMerica/Config/YourInteceptor.java
@@ -3,6 +3,8 @@ package org.perscholas.BankOfAntMerica.Config;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.perscholas.BankOfAntMerica.Security.AuthenticatedUserUtils;
+import org.perscholas.BankOfAntMerica.database.Entity.User;
 import org.perscholas.BankOfAntMerica.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
@@ -15,13 +17,26 @@ import org.springframework.web.servlet.ModelAndView;
 public class YourInteceptor implements HandlerInterceptor {
     @Autowired
     private UserService userService;
+    @Autowired
+    private AuthenticatedUserUtils authenticatedUserUtils;
 
-
+@Override
+public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    User currentUser = authenticatedUserUtils.getCurrentUserObject();
+    if (currentUser != null) {
+        request.setAttribute("currentUser", currentUser);
+        log.info("Current user: {}" , currentUser);
+        return true;
+    }
+    return false;
+}
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) {
         boolean isSafari = isSafari(request);
         modelAndView.addObject("isSafari", isSafari);
         log.debug("isSafari:{}", isSafari);
+
+
     }
     private boolean isSafari(HttpServletRequest request) {
         String userAgent = request.getHeader("User-Agent");
